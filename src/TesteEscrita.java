2f8b757c4370e61720febe67d0d7d01f44a8e199
GuiLaTT <guilherme.oliveira182@yahoo.com.br>
2023-02-15 08:25:28 -0300

    OutPutStream

---
+++ src/TesteEscrita.java
@@ -0,0 +1,23 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita {
+    public static void main(String[] args) throws IOException {
+        //Fluxo de entrada de arquivo
+
+        OutputStream fos = new FileOutputStream("lorem2.txt");
+        Writer isr = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(isr);
+
+        bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        bw.newLine();
+        bw.newLine();
+        bw.write("dashudsauda dhasudashduasdhu dashudsahuhdasu");
+
+        bw.close();
+    }
+}
